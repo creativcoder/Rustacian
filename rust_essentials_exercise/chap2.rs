80c18a4b2d5514336f624b4010d60a55801c5d19
Rahul Sharma <rsconceptx@gmail.com>
2015-09-12 01:43:29 +0530

    Update chap2.rs

--- rust_essentials_exercise/chap2.rs
+++ rust_essentials_exercise/chap2.rs
@@ -1,17 +1,23 @@
 use std::f32::consts;
+use std::env;
+
 static RAD : f32 = 3.14;
 
 mod circle_util {
 pub fn get_radius() -> Option<f32> {
 Some(3.45)
 }
-
 }
 
 fn main() {
-let rad = ::circle_util::get_radius().unwrap_or(RAD);
-println!("The Area of Circle is {:?} (m)^2",consts::PI*rad*rad);
 
-println!("{:?}",u32::from_str_radix(&"34",10).unwrap());
-
+	let mut argv = env::args();
+	let arg: String = argv.nth(1).unwrap();
+	let env_rad : i32 = arg.parse().unwrap();
+	
+	// :: -> indicates the root crate, (this src file)
+	let rad = ::circle_util::get_radius().unwrap_or(RAD);
+	println!("The Area of Circle is {:?} (m)^2",consts::PI * rad * rad);
+	println!("The Area of Circle is {:?} (m)^2",consts::PI * env_rad * env_rad);
+	println!("{:?}", u32::from_str_radix(&"34",10).unwrap());
 }
