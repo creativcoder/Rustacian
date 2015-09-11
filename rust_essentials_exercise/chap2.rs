c44c78ab06762941cada109842b15325f721c45c
Rahul Sharma <rsconceptx@gmail.com>
2015-09-12 01:35:53 +0530

    refactored method inside mod

--- rust_essentials_exercise/chap2.rs
+++ rust_essentials_exercise/chap2.rs
@@ -1,12 +1,17 @@
 use std::f32::consts;
-
 static RAD : f32 = 3.14;
 
-fn get_radius() -> Option<f32> {
+mod circle_util {
+pub fn get_radius() -> Option<f32> {
 Some(3.45)
 }
 
+}
+
 fn main() {
-let rad = get_radius().unwrap_or(RAD);
+let rad = ::circle_util::get_radius().unwrap_or(RAD);
 println!("The Area of Circle is {:?} (m)^2",consts::PI*rad*rad);
+
+println!("{:?}",u32::from_str_radix(&"34",10).unwrap());
+
 }
