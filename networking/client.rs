29aec72507445a1a090e0ae80671e3b4b058c0e6
Rahul Sharma <rsconceptx@gmail.com>
2015-09-13 00:23:39 +0530

    Added basic server and client

--- networking/client.rs
+++ networking/client.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 fn main() {
 
-let mut stream = TcpStream::connect("127.0.0.1:34254").unwrap();
+let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
 let _ = stream.write(&[1]);
 let _ = stream.read(&mut [0;128]);
 
---
+++ networking/server.rs
@@ -0,0 +1,23 @@
+use std::net::{TcpListener,TcpStream};
+use std::thread;
+
+fn main(){
+
+let server =  TcpListener::bind("127.0.0.1:3000").unwrap();
+
+fn handle_client(stream:TcpStream) {
+	println!("Accepted connection");
+}
+for stream in server.incoming() {
+	match stream {
+		Ok(stream) => {
+			thread::spawn(move|| {
+				handle_client(stream)
+			});
+		},
+		Err(e) => {println!("Connection Error");}
+	}
+}
+drop(server);
+
+}
