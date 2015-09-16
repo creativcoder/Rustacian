e723a8d22f576f778a3a12f5de73610cc78aac91
Rahul Sharma <rsconceptx@gmail.com>
2015-09-17 00:08:20 +0530

    updated client and server

--- networking/server.rs
+++ networking/server.rs
@@ -5,14 +5,14 @@ fn main(){
 
 let server =  TcpListener::bind("127.0.0.1:3000").unwrap();
 
-fn handle_client(stream:TcpStream) {
-	println!("Accepted connection");
+fn handle_client(stream:&TcpStream) {
+	println!("Accepted connection from {:?}",stream.local_addr().unwrap());
 }
 for stream in server.incoming() {
 	match stream {
 		Ok(stream) => {
 			thread::spawn(move|| {
-				handle_client(stream)
+				handle_client(&stream)
 			});
 		},
 		Err(e) => {println!("Connection Error");}
