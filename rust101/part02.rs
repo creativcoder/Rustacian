cad464f0eca93be4daf568da799e8c88248b1f48
Rahul Sharma <rsconceptx@gmail.com>
2015-10-05 10:53:11 +0530

    updated part02.rs with impls for floats

--- rust101/part02.rs
+++ rust101/part02.rs
@@ -1,46 +1,22 @@
-// Refactored previous part01.rs to trait impls and generic types
+// implementing generic traits for vec_min and added trait impls for floats
 
 pub enum SomethingOrNothing<T> {
 	Something(T),
 	Nothing,
 }
 
-// aliasing for Number ADT's
+use self::SomethingOrNothing::*;
+
+// taking aliases for some instances of generic type
 type NumberOrNothing = SomethingOrNothing<i32>;
-
-pub use self::SomethingOrNothing::*;
-pub use self::NumberOrNothing::*;
-
-
+type FloatNumOrNothing = SomethingOrNothing<f32>;
 
 impl<T> SomethingOrNothing<T> {
-
 	fn new(o:Option<T>) -> Self {
 		match o {None => Nothing,Some(t) => Something(t) }
 	}
-
 	fn to_option(self) -> Option<T> {
-		match self{
-		Nothing => None,
-		Something(t)=>Some(t),
-	}
-	}
-}
-
-impl Minimum for i32 {
-    fn min(self, b: Self) -> Self {
-        if self < b { self } else { b }
-    }
-}
-
-
-impl NumberOrNothing {
-
-	fn print_min(self){
-		match self {
-			Nothing => println!("The list is empty"),
-			Something(min) => println!("The minimum from the list is {}",min),
-		}
+		match self { Nothing => None,Something(t) => Some(t) }
 	}
 }
 
@@ -48,28 +24,61 @@ pub trait Minimum : Copy {
 	fn min(self,b:Self) -> Self;
 }
 
-pub fn vec_min <T:Minimum> (v:Vec<T>) -> SomethingOrNothing<T> {
+pub fn vec_min <T:Minimum>(v:Vec<T>) -> SomethingOrNothing<T> {
 	let mut min = Nothing;
 	for e in v {
-		min = Something(match min{Nothing => e,Something(n)=>e.min(n)})
+		min = Something(match min {
+			Nothing => e,
+			Something(n) => e.min(n),
+		});
 	}
 	min
 }
 
-
-fn read_vec(rang:u32) -> Vec<i32> {
-	match rang {
-		0 => {vec![]},
-		_ => {vec![87,4,7,6,5,9,453]},
+impl Minimum for i32 {
+	fn min(self,b:Self) -> Self {
+		if self < b {self} else {b}
 	}
 }
 
-pub fn main(){
-	let vec_list = read_vec(8);
-	let min = vec_min(vec_list);
-
-	min.print_min();
+impl Minimum for f32 {
+	fn min(self,b:Self) -> Self {
+		if self < b {self} else {b}
+	}
 }
 
 
+impl NumberOrNothing { 
+	pub fn print(self) {
+		match self {
+			Nothing => println!("The nunber is : <nothing>"),
+			Something(n) => println!("The number is {}",n),
+		};
+	}
+}
 
+impl FloatNumOrNothing { 
+	pub fn print(self) {
+		match self {
+			Nothing => println!("The nunber is : <nothing>"),
+			Something(n) => println!("The number is {}",n),
+		};
+	}
+}
+
+fn read_vec_int() -> Vec<i32> {
+	vec![18,5,7,3,9,27]
+}
+
+fn read_vec_float() -> Vec<f32> {
+	vec![18.4,5.2,7.35,66.99,9.2,27.23]
+}
+
+pub fn main() {
+	let vec_int = read_vec_int();
+	let vec_float = read_vec_float();
+	let min_int = vec_min(vec_int);
+	let min_float = vec_min(vec_float);
+	min_int.print();
+	min_float.print();
+}
