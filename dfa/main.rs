90bc9f6b12b3bb7ce7c042b50216ec0abd4ce60b
Rahul Sharma <rsconceptx@gmail.com>
2015-10-25 00:30:00 +0530

    Update main.rs

--- dfa/main.rs
+++ dfa/main.rs
@@ -1,7 +1,8 @@
-fn gen_regex_obj(pat:&String){
+mod automata;
 
-}
+use automata::even_zeros_dfa;
+
 fn main() {
-  let pat = "aa*bba";
-  
+    let string = "101111111111000".to_string();
+    even_zeros_dfa::init_fsm(&string);
 }
