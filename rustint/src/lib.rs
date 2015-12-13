582c1557f7dc2a7a1519bc8fc313a4c5235d2470
creativcoder <rsconceptx@gmail.com>
2015-12-14 00:37:49 +0530

    sum operator implemented in rustint

--- rustint/src/lib.rs
+++ rustint/src/lib.rs
@@ -1,6 +1,4 @@
 #![feature(str_char)]
-
-
 use std::ops::Add;
 
 #[derive(Debug)]
@@ -9,8 +7,8 @@ pub struct BigInt {
 }
 
 impl BigInt {
-    fn new() -> Self {
-        BigInt { val:Some(Vec::with_capacity(1000)) }
+    pub fn new(precision:usize) -> Self {
+        BigInt { val:Some(Vec::with_capacity(precision)) }
     }
     pub fn from_str(num_str:&str) -> Self {
         let mut val:Vec<u64> = Vec::with_capacity(1000); 
@@ -21,32 +19,33 @@ impl BigInt {
         }
         BigInt {val:Some(val) }
     }
+    pub fn display(&self) {
+        let mut num = self.val.clone().unwrap();
+        num.reverse();
+        for i in num {
+            print!("{:?}",i );
+        }
+        println!("");
+    }
 }
 
 impl Add for BigInt {
     type Output = BigInt;
     fn add(self,_rhs:BigInt) -> BigInt {
         let mut carry = 0;
-        let mut result = BigInt::new();
         let lhs = self.val.clone().unwrap();
         let rhs = _rhs.val.clone().unwrap();
-        if rhs.len() <= lhs.len() {
-            for i in 0..rhs.len() {
-                if let Some(ref mut val) = result.val {
-                    val[i] = ((lhs[i]+rhs[i])%10);
-                    carry = (lhs[i]+rhs[i])/10;
-                    val.push(carry);
-                }
-            }
-        } else {
-            for i in 0..lhs.len() {
-                if let Some(ref mut val) = result.val {
-                    val[i] = ((lhs[i]+rhs[i])%10);
-                    carry = (lhs[i]+rhs[i])/10;
-                    val.push(carry);
-                }
-            }
+        let smaller = if lhs.len()<rhs.len() {lhs.len()} else {rhs.len()};
+        let mut result = {if lhs.len() > rhs.len() {lhs.clone()}  else {rhs.clone()}};
+        for i in 0..smaller {
+            result[i] = (lhs[i] + rhs[i])%10 + carry;
+            carry = {if lhs[i] + rhs[i] > 9 {1} else {0}};
         }
-    result    
+        if carry == 1 {
+            result[smaller] +=1;
+        }
+    BigInt {val:Some(result) }
     }
 }
+
+
--- rustint/src/main.rs
+++ rustint/src/main.rs
@@ -1,11 +1,15 @@
 extern crate rustint;
 
 use rustint::BigInt;
-use std::ops::Add;
 
 fn main() {
-   let b = BigInt::from_str("9999");
-   let b2 = BigInt::from_str("99");
-   let t = b + b2;
-   println!("{:?}",t);
+   
+   let big_lhs = BigInt::from_str("324353453243234235345345634543423243432433453453435");
+   let big_rhs = BigInt::from_str("9934554345432423442423434534534534325");
+   let small_lhs = BigInt::from_str("111");
+   let small_rhs = BigInt::from_str("22992");
+   let sum_big = big_lhs + big_rhs;
+   let sum_small = small_lhs + small_rhs;
+   sum_big.display();
+   sum_small.display();
 }
